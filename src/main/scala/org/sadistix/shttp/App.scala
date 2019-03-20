50e6d7fb4ec9f6200ad6f63328c5cfd3b48ae41b
Wonho ha <haho013039@gmail.com>
2019-03-20 16:47:49 +0900

    Update App.scala

--- src/main/scala/org/sadistix/shttp/App.scala
+++ src/main/scala/org/sadistix/shttp/App.scala
@@ -1,12 +1,17 @@
-import java.io.File
+package org.sadistix.shttp
 
-import org.http4s.{HttpRoutes, StaticFile}
+import java.io.File
+import java.util.concurrent.Executors
+
+import org.http4s.{HttpRoutes, Response, StaticFile}
 import org.http4s.dsl.io._
 import org.http4s.server.blaze._
+import org.http4s.twirl._
 import java.util.concurrent._
 
 import doobie.ExecutionContexts
 import doobie.hikari.HikariTransactor
+import doobie.hikari._
 import cats.effect._
 import cats.implicits._
 import doobie.implicits._
@@ -14,10 +19,14 @@ import doobie.implicits._
 // static file
 import scala.concurrent.ExecutionContext
 
+import org.log4s
+
 object App extends IOApp {
 
   val blockingEc = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(100))
   implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+
+  val logger = log4s.getLogger("LasLogger")
 
   val transactor: Resource[IO, HikariTransactor[IO]] =
     for {
@@ -33,7 +42,6 @@ object App extends IOApp {
       )
     } yield xa
 
-  var count: Int = 0
   var is_exist: Int = 0
 
   val helloWorldService = HttpRoutes.of[IO] {
@@ -42,11 +50,18 @@ object App extends IOApp {
         transactor.use {
           xa => sql"create table Loves(love integer(11), username varchar(20))".update.run.transact(xa)
         }.unsafeRunSync()
+        is_exist += 1
+      }
+      StaticFile
+        .fromFile(new File("resource/main.html"), blockingEc, Some(request))
+        .getOrElseF(NotFound())
+    }
+    case request@GET -> Root / name => {
+      if (is_exist == 0) {
         transactor.use {
-          xa => sql"insert into Loves values(0, 'las')".update.run.transact(xa)
+          xa => sql"create table Loves(love integer(11), username varchar(20))".update.run.transact(xa)
         }.unsafeRunSync()
         is_exist += 1
-        count = 0
       }
       StaticFile
         .fromFile(new File("resource/main.html"), blockingEc, Some(request))
@@ -57,15 +72,21 @@ object App extends IOApp {
         transactor.use {
           xa => sql"create table Loves(love integer(11), username varchar(20))".update.run.transact(xa)
         }.unsafeRunSync()
+        is_exist += 1
+      }
+      Ok(transactor.use {
+        xa => sql"select love from Loves where username='admin' limit 1".query[Int].unique.transact(xa)
+      }.unsafeRunSync().toString())
+    }
+    case request@GET -> Root / "heart" / name => {
+      if (is_exist == 0) {
         transactor.use {
-          xa => sql"insert into Loves values(0, 'las')".update.run.transact(xa)
+          xa => sql"create table Loves(love integer(11), username varchar(20))".update.run.transact(xa)
         }.unsafeRunSync()
         is_exist += 1
-        count = 0
       }
-
       Ok(transactor.use {
-        xa => sql"select love from Loves where username='las' limit 1".query[Int].unique.transact(xa)
+        xa => sql"select love from Loves where username=$name limit 1".query[Int].unique.transact(xa)
       }.unsafeRunSync().toString())
     }
     case request@GET -> Root / "love" => {
@@ -73,17 +94,38 @@ object App extends IOApp {
         transactor.use {
           xa => sql"create table Loves(love integer(11), username varchar(20))".update.run.transact(xa)
         }.unsafeRunSync()
-        transactor.use {
-          xa => sql"insert into Loves values(0, 'las')".update.run.transact(xa)
-        }.unsafeRunSync()
         is_exist += 1
-        count = 0
       }
       transactor.use {
-        xa => sql"update Loves set love=love+1 where username='las'".update.run.transact(xa)
+        xa => sql"update Loves set love=love+1 where username='admin'".update.run.transact(xa)
       }.unsafeRunSync()
-      count += 1
       Ok()
+    }
+    case request@GET -> Root / "love" / name => {
+      if (is_exist == 0) {
+        transactor.use {
+          xa => sql"create table Loves(love integer(11), username varchar(20))".update.run.transact(xa)
+        }.unsafeRunSync()
+        is_exist += 1
+      }
+      transactor.use {
+        xa => sql"update Loves set love=love+1 where username=$name".update.run.transact(xa)
+      }.unsafeRunSync()
+      Ok()
+    }
+    case request@GET -> Root / "user" / name => {
+
+      if (is_exist == 0) {
+        transactor.use {
+          xa => sql"create table Loves(love integer(11), username varchar(20))".update.run.transact(xa)
+        }.unsafeRunSync()
+        is_exist += 1
+      }
+      logger.info("User " + name + " is detected")
+      transactor.use {
+        xa => sql"insert into Loves values(0, ${name})".update.run.transact(xa)
+      }.unsafeRunSync()
+      Ok("123")
     }
   }.orNotFound
 
